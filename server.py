52759c42aa19fa4d2014ce74b30b6333c6b1efc7
Awais Kamran <awaiskamran@Aribas-MacBook-Pro.local>
2023-12-28 13:59:21 +0500

    feat: server file updated with failed status updates.

--- server.py
+++ server.py
@@ -49,7 +49,9 @@ origins = [
     "http://localhost",
     "http://localhost:4200",
     "https://openai-chatbot-interface-9ab52001491e.herokuapp.com",
+    "https://openai-chatbot-interface-9ab52001491e.herokuapp.com/"
     "https://voice-chat-bot-client-18687526ee9a.herokuapp.com"
+    "https://voice-chat-bot-client-18687526ee9a.herokuapp.com/"
 ]
 # Enable All External Links
 # origins = ["*"]
